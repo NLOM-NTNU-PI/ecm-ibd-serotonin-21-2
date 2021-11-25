b1c0e37ed76946a1341af403fc5cca687e929534
Magnus Lilledahl <29771060+NLOM-NTNU-PI@users.noreply.github.com>
2021-11-25 10:42:37 +0100

    Created serotonin_21_2_quantitative_analysis. Thresholding methods. Updated .gitignore to exclude .zip files

---
+++ serotonin_21_2_quantitative_analysis.m
@@ -0,0 +1,48 @@
+%The goal of this analysis is to investigate possbile quantitative measures
+%of collagen content.
+%Project: IBD-serotonin (onedrive)
+%Experiment:serotonin-21-2 (onedrive)
+%Samples: rat intestines from Atle van Belen Granlund.
+%Data:serotonin-21-2 (aurora)
+
+close all %close all figures
+
+%Place data (.lif file) in  folder .\data
+%Load data
+if ~(exist('data','var')) %load data unless already loaded
+    data = bfopen('.\data\serotonin-21-2.lif');
+end
+%data is an nx4 cell array of n rows of images where the image data are stored in
+%the first position of the second argument. The other columns are metadata.
+
+%Choose image
+n = 8;
+img = data{n,1};
+
+%Select individual channels
+dapi = img{1,1};
+shg = img{2,1};
+af = img{3,1};
+
+image(shg,'CDataMapping','scaled')
+
+%Otsus thresholding method
+figure
+level = graythresh(shg);
+shg_bw = imbinarize(shg,level);
+figure
+imshowpair(shg_bw,shg,'montage')
+
+%Multilevel Otsus
+figure
+levels = multithresh(shg,2);
+shg_seg = imquantize(shg,levels);
+shg_seg_rgb = label2rgb(shg_seg);
+imshowpair(shg_seg_rgb,shg,'montage')
+
+%Adaptive threshold
+figure
+adaptive_threshold = adaptthresh(shg);
+shg_adapt = imbinarize(shg,adaptive_threshold); %Only when features throughout image
+imshowpair(shg_adapt,shg,'montage')
+
