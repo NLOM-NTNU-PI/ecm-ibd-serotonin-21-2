9c1cd06fb77237d6eebf05b6cd1128c1667edb12
Magnus Lilledahl <29771060+NLOM-NTNU-PI@users.noreply.github.com>
2021-11-25 10:42:37 +0100

    Updated script to analyse and save all images in experiment

--- serotonin_21_2_analysis.m
+++ serotonin_21_2_analysis.m
@@ -2,7 +2,7 @@
 %visualization.
 %%%
 %Data on aurora: serotonin-21-2
-%Experiment notes on github 
+%Experiment journal on github
 %https://github.com/NLOM-NTNU-PI/labbook/blob/fceed934a88b045be3612a74409b89a18d587cda/IBD-serotonin
 %%%
 
@@ -11,46 +11,48 @@
 if ~(exist('data','var')) %load data unless already loaded
     data = bfopen('.\data\serotonin-21-2.lif');
 end
-%data is an nx4 cell array of n images where the image data are stored in
-%the first position of the second argument. The other rows are metadata.
+%data is an nx4 cell array of n rows of images where the image data are stored in
+%the first position of the second argument. The other columns are metadata.
 
-%Pick out a particular image
-%n = {1,2} are test images with less than 3 channels.
-n = 5; %image number in series
-img = data{n,1}; 
-display(img{1,2}) %show image project information
-
-%Project naming information in imag{x,2}
-%Select individual channels
-dapi = img{1,1};
-shg = img{2,1};
-af = img{3,1};
-%Process images
+for n = 3:size(data,1) %n = {1,2} are test images 
+    img = data{n,1};
+    
+    %Get image project name
+    metadata = strtrim(split(img{1,2},';')); %Project naming information in imag{x,2},
+    name = metadata{2};
+    %display(img{1,2}) %Show image project information to relate to journal notes
+    
+    %Select individual channels
+    dapi = img{1,1};
+    shg = img{2,1};
+    af = img{3,1};
+    
+    %Process images
     %DAPI
     Pdapi = medfilt2(dapi); %median filter %other options: gaussian, wiener..
-    Pdapi(Pdapi<4000) = 0; %threshold to remove autofluorescence leakage
-
+    Pdapi(Pdapi<4000) = 0; %threshold to remove autofluorescence leakage    
     %SHG
     Pshg = medfilt2(shg);
     Pshg(Pshg<2000) = 0; %threshold to remove autofluorescence leakage
-    %Pshg(Pdapi~=0) = 0; %threshold where cell nuclei is present
-    
+    %Pshg(Pdapi~=0) = 0; %threshold where cell nuclei is present    
     %AF
     Paf = medfilt2(af);
-    Paf(Paf<400) = 0;
-    %imshowpair(af,Paf,'montage')
-
+    if n < 10
+        Paf(Paf<400) = 0;
+    end
+        
     %Adjust intensity levels
     Pdapi = imadjust(Pdapi);
     Pshg = imadjust(Pshg);
-        Paf_high =  double(max(Paf(:)))/65536;
+    Paf_high =  double(max(Paf(:)))/65536;
     Paf = imadjust(Paf,[0,Paf_high],[0,.3]);
     
-%Create rgb image
-%r = uint8(zeros(512,512));
-r = Paf;
-g = Pshg;
-b = Pdapi;
-rgb = cat(3,r,g,b);
-imwrite(rgb, './results/img.png')
-%image(rgb)
+    %Create rgb image and save to file
+    r = Paf;
+    g = Pshg;
+    b = Pdapi;
+    rgb = cat(3,r,g,b);
+    
+    fname = ['./results/' name '-processed.png'];
+    imwrite(rgb, fname)
+end
