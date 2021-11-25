bab4d139faaecf7f717eb436ec37f4dc0d84195f
Magnus Lilledahl <29771060+NLOM-NTNU-PI@users.noreply.github.com>
2021-11-25 10:42:37 +0100

    worked on processing and saving of images. updated gitignore file

--- serotonin_21_2_analysis.m
+++ serotonin_21_2_analysis.m
@@ -1,17 +1,56 @@
+%This files processes data from experiment serotonin-21-2 for
+%visualization.
+%%%
+%Data on aurora: serotonin-21-2
+%Experiment notes on github 
+%https://github.com/NLOM-NTNU-PI/labbook/blob/fceed934a88b045be3612a74409b89a18d587cda/IBD-serotonin
+%%%
+
+%Place data (.lif file) in  folder .\data
+%Load data
 if ~(exist('data','var')) %load data unless already loaded
     data = bfopen('.\data\serotonin-21-2.lif');
 end
+%data is an nx4 cell array of n images where the image data are stored in
+%the first position of the second argument. The other rows are metadata.
 
+%Pick out a particular image
+%n = {1,2} are test images with less than 3 channels.
 n = 5; %image number in series
-img = data{n,1};
+img = data{n,1}; 
+display(img{1,2}) %show image project information
 
-r = uint8(zeros(512,512));
-r = img{3,1};
-g = img{2,1};
-b = img{1,1};
+%Project naming information in imag{x,2}
+%Select individual channels
+dapi = img{1,1};
+shg = img{2,1};
+af = img{3,1};
+%Process images
+    %DAPI
+    Pdapi = medfilt2(dapi); %median filter %other options: gaussian, wiener..
+    Pdapi(Pdapi<4000) = 0; %threshold to remove autofluorescence leakage
 
-r = imadjust(r,[0.005,0.01],[0,.3]);
-b = imadjust(b);
+    %SHG
+    Pshg = medfilt2(shg);
+    Pshg(Pshg<2000) = 0; %threshold to remove autofluorescence leakage
+    %Pshg(Pdapi~=0) = 0; %threshold where cell nuclei is present
+    
+    %AF
+    Paf = medfilt2(af);
+    Paf(Paf<400) = 0;
+    %imshowpair(af,Paf,'montage')
 
+    %Adjust intensity levels
+    Pdapi = imadjust(Pdapi);
+    Pshg = imadjust(Pshg);
+        Paf_high =  double(max(Paf(:)))/65536;
+    Paf = imadjust(Paf,[0,Paf_high],[0,.3]);
+    
+%Create rgb image
+%r = uint8(zeros(512,512));
+r = Paf;
+g = Pshg;
+b = Pdapi;
 rgb = cat(3,r,g,b);
-image(rgb)
+imwrite(rgb, './results/img.png')
+%image(rgb)
